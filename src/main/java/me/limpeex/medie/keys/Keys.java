190e49ba9e39be3b31e8be553f4b75209f67947f
limpeex <84831069+limpeex@users.noreply.github.com>
2022-06-04 21:11:11 +0300

    Create Keys.java

---
+++ src/main/java/me/limpeex/medie/keys/Keys.java
@@ -0,0 +1,10 @@
+package me.limpeex.medievalplugin.keys;
+
+import me.limpeex.medievalplugin.MedievalPlugin;
+import org.bukkit.NamespacedKey;
+
+public class Keys {
+    public static NamespacedKey badgeKey = new NamespacedKey(MedievalPlugin.plugin, "badges");
+    public static NamespacedKey ironConverterKey = new NamespacedKey(MedievalPlugin.plugin, "iron-converter");
+    public static NamespacedKey chestKeys = new NamespacedKey(MedievalPlugin.plugin, "chests");
+}
